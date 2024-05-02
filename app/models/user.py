b721b8d4a8173467524d2db7e6f086cd8f58a16c
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2024-05-01 21:34:11 -0300

    se agregan modelos user y user_data

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from app import db
+from dataclasses import dataclass
+from .user_data import UserData
+
+@dataclass
+class User(db.Model):
+    __tablename__ = 'users'
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username: str = db.Column(db.String(120), unique=True, nullable=False)
+    email: str = db.Column(db.String(120), nullable=False)
+    password: str = db.Column(db.String(120), unique=True, nullable=False)
+    userdata = db.relationship('UserData', back_populates='user', uselist=False)
+
+    def __init__(self, user_data: UserData = None):
+        self.data = user_data
---
+++ app/models/user_data.py
@@ -0,0 +1,13 @@
+from app import db
+from dataclasses import dataclass
+
+@dataclass
+class UserData(db.Model):
+    __tablename__ = 'users_data'
+    id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    firstname: str = db.Column(db.String(120), nullable=False)
+    lastname: str = db.Column(db.String(120), nullable=False)
+    phone: str = db.Column(db.String(120), nullable=False)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship("User", back_populates='data', uselist=False)
+
