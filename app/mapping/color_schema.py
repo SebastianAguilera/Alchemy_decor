e609c36b297b0018cfd07a2c8082c18d2afd4337
mjuarez713 <martinjuarez1302@gmail.com>
2024-05-08 17:22:31 -0300

    se agregan las clases product_schema, color_schema, product_color_schema

---
+++ app/mapping/color_schema.py
@@ -0,0 +1,15 @@
+from app.models import Color
+from marshmallow import validate, Schema, fields, post_load
+
+class ColorSchema(Schema):
+    id = fields.Integer(dump_only=True)
+    name = fields.String(required=True, validate=validate.Length(min=2, max=120))
+    description = fields.String(required=True, validate=validate.Length(min=2, max=120))
+
+    @post_load
+    def make_color(self, data, **kwargs):
+        return Color(**data)
+    
+
+
+
