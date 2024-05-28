3478b160c0af2e16f3b7443d27f5473c43010d5f
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2024-05-27 22:33:05 -0300

    cambios en los tests

--- test/test_product.py
+++ test/test_product.py
@@ -1,44 +1,34 @@
 import unittest
 from flask import current_app
 from app import create_app, db
-from app.models.product import Product
+from app.models import Product
 
 class ProductTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app()
-        self.app.config['TESTING'] = True
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        db.create_all()
 
-        self.PRODUCT_ID = 1
-        self.PRODUCT_NAME = 'Oak Table'
-        self.PRODUCT_DESCRIPTION = 'A sturdy oak table, 2x1m'
-        self.PRODUCT_PRICE = 700.00
-        self.PRODUCT_STOCK = 10
+  def setUp(self):
+    self.app = create_app()
+    self.app_context = self.app.app_context()
+    self.app_context.push()
+    
+  def tearDown(self):
+    self.app_context.pop()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
+  def test_app(self):
+      self.assertIsNotNone(current_app)
 
-    def test_app(self):
-        self.assertIsNotNone(current_app)
+  def test_product(self):
+      product = Product()
+      product.id = 10
+      product.name = 'test'
+      product.description = 'hola'
+      product.price = 10.5
+      product.stock = 10
 
-    def test_product(self):
-        product = Product()
-        product.id = self.PRODUCT_ID
-        product.name = self.PRODUCT_NAME
-        product.description = self.PRODUCT_DESCRIPTION
-        product.price = self.PRODUCT_PRICE
-        product.stock = self.PRODUCT_STOCK
-
-        self.assertEqual(product.id, self.PRODUCT_ID)
-        self.assertEqual(product.name, self.PRODUCT_NAME)
-        self.assertEqual(product.description, self.PRODUCT_DESCRIPTION)
-        self.assertEqual(product.price, self.PRODUCT_PRICE)
-        self.assertEqual(product.stock, self.PRODUCT_STOCK)
+      self.assertEqual(product.id, 10)
+      self.assertEqual(product.name, 'test')
+      self.assertEqual(product.description, 'hola')
+      self.assertEqual(product.price, 10.5)
+      self.assertEqual(product.stock, 10)
 
 if __name__ == '__main__':
     unittest.main()
