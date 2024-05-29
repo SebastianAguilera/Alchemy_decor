b350ce8b9a1eab8242232d1d833c0b09cd9b1361
mjuarez713 <martinjuarez1302@gmail.com>
2024-05-29 17:46:47 -0300

    se edita el archivo init de models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,9 +2,10 @@ from .user import User
 from .user_data import UserData
 from .product import Product
 from .category import Category
-"""
-from .product_color import ProductColor
 from .color import Color
 from .cart import Cart
+
+"""
 from .cart_product import CartProduct
+from .product_color import ProductColor
 """
