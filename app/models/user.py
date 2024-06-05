a06911fada14cec58f925245c3b58faf2f42bce4
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2024-06-05 17:51:20 -0300

    se agrega CRUD de user utilizando Active Record

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from .user_data import UserData
+from typing import List
 from app import db
 
 @dataclass
@@ -9,9 +10,31 @@ class User(db.Model):
     username: str = db.Column(db.String(120), unique=True, nullable=False)
     email: str = db.Column(db.String(120), nullable=False)
     password: str = db.Column(db.String(120), unique=True, nullable=False)
-
     data = db.relationship('UserData', back_populates='user', uselist=False)
-  
+
     def __init__(self, user_data: UserData = None):
         self.data = user_data
+
+    def save(self) -> 'User':
+        db.session.add(self)
+        db.session.commit()
+        return self
+    
+    def delete(self) -> None:
+        db.session.delete(self)
+        db.session.commit()
+    
+    @classmethod
+    def all(cls) -> List['User']:
+        return cls.query.all()
+    
+    @classmethod
+    def find(cls, id: int) -> 'User':
+        return cls.query.get(id)
+    
+    @classmethod
+    def find_by(cls, **kwargs) -> List['User']:
+        return cls.query.filter_by(**kwargs).all()
+    
+        
 
--- test/test_user.py
+++ test/test_user.py
@@ -52,6 +52,96 @@ class UserTestCase(unittest.TestCase):
     self.assertEqual(user.data.city, 'city')
     self.assertEqual(user.data.country,'country')
 
+
+  def test_user_save(self):
+    data = UserData()
+    data.firstname = 'firstname'
+    data.lastname = 'lastname'
+    data.phone = '123456789'
+    data.address = 'address'
+    data.city = 'city'
+    data.country = 'country'
+
+    user = User(data)
+    user.username = 'test'
+    user.email = 'test@test.com'
+    user.password = 'test1234'
+
+    user.save()
+
+    self.assertGreaterEqual(user.id, 1)
+    self.assertEqual(user.email, 'test@test.com')
+    self.assertEqual(user.username, 'test')
+    self.assertEqual(user.password, 'test1234')
+    self.assertIsNotNone(user.data)
+    self.assertEqual(user.data.firstname, 'firstname')
+    self.assertEqual(user.data.lastname, 'lastname')
+    self.assertEqual(user.data.phone, '123456789')
+    self.assertEqual(user.data.address, 'address')
+    self.assertEqual(user.data.city, 'city')
+    self.assertEqual(user.data.country,'country')
+
+  def test_user_delete(self):
+
+    data = UserData()
+    data.firstname = 'firstname'
+    data.lastname = 'lastname'
+    data.phone = '123456789'
+    data.address = 'address'
+    data.city = 'city'
+    data.country = 'country'
+
+    user = User(data)
+    user.username = 'test'
+    user.email = 'test@test.com'
+    user.password = 'test1234'
+
+    user.save()
+    user.delete()
+    self.assertIsNone(User.find(user.id))
+
+  def test_user_all(self):
+    data = UserData()
+    data.firstname = 'firstname'
+    data.lastname = 'lastname'
+    data.phone = '123456789'
+    data.address = 'address'
+    data.city = 'city'
+    data.country = 'country'
+
+    user = User(data)
+    user.username = 'test'
+    user.email = 'test@test.com'
+    user.password = 'test1234'
+
+    user.save()
+
+    users = User.all()
+    self.assertGreaterEqual(len(users), 1)
+
+  def test_user_find(self):
+
+    data = UserData()
+    data.firstname = 'firstname'
+    data.lastname = 'lastname'
+    data.phone = '123456789'
+    data.address = 'address'
+    data.city = 'city'
+    data.country = 'country'
+
+    user = User(data)
+    user.username = 'test'
+    user.email = 'test@test.com'
+    user.password = 'test1234'
+
+    user.save()
+
+    user_find = User.find(1)
+    self.assertIsNotNone(user_find)
+    self.assertEqual(user_find.id, user.id)
+    self.assertEqual(user_find.email, user.email)
+
+
 if __name__ == '__main__':
     unittest.main()
  
