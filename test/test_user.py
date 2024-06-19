41c310694e5a7e3ce289e5c2f72c6f7c58927b9c
Sebastián Aguilera <sebaalejandro.aguilera@gmail.com>
2024-06-19 16:47:41 -0300

    se aplica el principio DRY en el test_user

--- test/test_user.py
+++ test/test_user.py
@@ -12,6 +12,15 @@ class UserTestCase(unittest.TestCase):
   """
 
   def setUp(self):
+    self.USERNAME_PRUEBA = 'username'
+    self.EMAIL_PRUEBA = 'email'
+    self.PASSWORD_PRUEBA = 'password123'
+    self.FIRSTNAME_PRUEBA = 'firstname'
+    self.LASTNAME_PRUEBA = 'lastname'
+    self.PHONE_PRUEBA = '1234567890'
+    self.ADDRESS_PRUEBA = 'address'
+    self.CITY_PRUEBA = 'city'
+    self.COUNTRY_PRUEBA = 'country'
     self.app = create_app()
     self.app_context = self.app.app_context()
     self.app_context.push()
@@ -24,123 +33,69 @@ class UserTestCase(unittest.TestCase):
 
   def test_app(self):
     self.assertIsNotNone(current_app)
-
   
   def test_user(self):
-    
-    data = UserData()
-    data.firstname = 'firstname'
-    data.lastname = 'lastname'
-    data.phone = '123456789'
-    data.address = 'address'
-    data.city = 'city'
-    data.country = 'country'
-
-    user = User(data)
-    user.username = 'test'
-    user.email = 'test@test.com'
-    user.password = 'test1234'
-
-    self.assertEqual(user.email, 'test@test.com')
-    self.assertEqual(user.username, 'test')
-    self.assertEqual(user.password, 'test1234')
+    user = self.__get_user()
+    self.assertEqual(user.email, self.EMAIL_PRUEBA)
+    self.assertEqual(user.username, self.USERNAME_PRUEBA)
+    self.assertEqual(user.password, self.PASSWORD_PRUEBA)
     self.assertIsNotNone(user.data)
-    self.assertEqual(user.data.firstname, 'firstname')
-    self.assertEqual(user.data.lastname, 'lastname')
-    self.assertEqual(user.data.phone, '123456789')
-    self.assertEqual(user.data.address, 'address')
-    self.assertEqual(user.data.city, 'city')
-    self.assertEqual(user.data.country,'country')
+    self.assertEqual(user.data.firstname, self.FIRSTNAME_PRUEBA)
+    self.assertEqual(user.data.lastname, self.LASTNAME_PRUEBA)
+    self.assertEqual(user.data.phone, self.PHONE_PRUEBA)
+    self.assertEqual(user.data.address, self.ADDRESS_PRUEBA)
+    self.assertEqual(user.data.city, self.CITY_PRUEBA)
+    self.assertEqual(user.data.country, self.COUNTRY_PRUEBA)
 
 
   def test_user_save(self):
-    data = UserData()
-    data.firstname = 'firstname'
-    data.lastname = 'lastname'
-    data.phone = '123456789'
-    data.address = 'address'
-    data.city = 'city'
-    data.country = 'country'
-
-    user = User(data)
-    user.username = 'test'
-    user.email = 'test@test.com'
-    user.password = 'test1234'
-
+    user = self.__get_user()
     user.save()
-
     self.assertGreaterEqual(user.id, 1)
-    self.assertEqual(user.email, 'test@test.com')
-    self.assertEqual(user.username, 'test')
-    self.assertEqual(user.password, 'test1234')
+    self.assertEqual(user.email, self.EMAIL_PRUEBA)
+    self.assertEqual(user.username, self.USERNAME_PRUEBA)
+    self.assertEqual(user.password, self.PASSWORD_PRUEBA)
     self.assertIsNotNone(user.data)
-    self.assertEqual(user.data.firstname, 'firstname')
-    self.assertEqual(user.data.lastname, 'lastname')
-    self.assertEqual(user.data.phone, '123456789')
-    self.assertEqual(user.data.address, 'address')
-    self.assertEqual(user.data.city, 'city')
-    self.assertEqual(user.data.country,'country')
+    self.assertEqual(user.data.firstname, self.FIRSTNAME_PRUEBA)
+    self.assertEqual(user.data.lastname, self.LASTNAME_PRUEBA)
+    self.assertEqual(user.data.phone, self.PHONE_PRUEBA)
+    self.assertEqual(user.data.address, self.ADDRESS_PRUEBA)
+    self.assertEqual(user.data.city, self.CITY_PRUEBA)
+    self.assertEqual(user.data.country, self.COUNTRY_PRUEBA)
 
   def test_user_delete(self):
-
-    data = UserData()
-    data.firstname = 'firstname'
-    data.lastname = 'lastname'
-    data.phone = '123456789'
-    data.address = 'address'
-    data.city = 'city'
-    data.country = 'country'
-
-    user = User(data)
-    user.username = 'test'
-    user.email = 'test@test.com'
-    user.password = 'test1234'
-
+    user = self.__get_user()
     user.save()
     user.delete()
     self.assertIsNone(User.find(user.id))
 
   def test_user_all(self):
-    data = UserData()
-    data.firstname = 'firstname'
-    data.lastname = 'lastname'
-    data.phone = '123456789'
-    data.address = 'address'
-    data.city = 'city'
-    data.country = 'country'
-
-    user = User(data)
-    user.username = 'test'
-    user.email = 'test@test.com'
-    user.password = 'test1234'
-
+    user = self.__get_user()
     user.save()
-
     users = User.all()
     self.assertGreaterEqual(len(users), 1)
 
   def test_user_find(self):
-
-    data = UserData()
-    data.firstname = 'firstname'
-    data.lastname = 'lastname'
-    data.phone = '123456789'
-    data.address = 'address'
-    data.city = 'city'
-    data.country = 'country'
-
-    user = User(data)
-    user.username = 'test'
-    user.email = 'test@test.com'
-    user.password = 'test1234'
-
+    user = self.__get_user()
     user.save()
-
     user_find = User.find(1)
     self.assertIsNotNone(user_find)
     self.assertEqual(user_find.id, user.id)
     self.assertEqual(user_find.email, user.email)
 
+  def __get_user(self): 
+    data = UserData()
+    data.firstname = self.FIRSTNAME_PRUEBA
+    data.lastname = self.LASTNAME_PRUEBA
+    data.phone = self.PHONE_PRUEBA
+    data.address = self.ADDRESS_PRUEBA
+    data.city = self.CITY_PRUEBA
+    data.country = self.COUNTRY_PRUEBA
+    user = User(data)
+    user.username = self.USERNAME_PRUEBA
+    user.email = self.EMAIL_PRUEBA
+    user.password = self.PASSWORD_PRUEBA
+    return user
 
 if __name__ == '__main__':
     unittest.main()
